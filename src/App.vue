ebe5bca897e8d63575881893fa9a68ed5a87cf0c
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-06-26 16:56:34 +0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Stepper previousPage="cart" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Stepper from './components/Stepper.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Stepper
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+body {
+  background-color: #fff8d6;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 20px;
 }
 </style>
---
+++ src/components/Stepper.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <h4>Back to {{ previousPage }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Stepper',
+  props: {
+    previousPage: String
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.container
+  border-radius 10px
+  background-color white
+  min-height 200px
+  padding 20px
+  -webkit-box-shadow: 3px 6px 17px -3px rgba(249,81,23,0.61)
+  box-shadow: 3px 6px 17px -3px rgba(249,81,23,0.61)
+</style>

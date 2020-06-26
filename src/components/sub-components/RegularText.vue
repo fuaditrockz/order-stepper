03c2f35c82a1f974090d2871f0b71619b8888f9b
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-06-27 00:40:44 +0700

    [RegularText.vue] Add component for regular text.

---
+++ src/components/sub-components/RegularText.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div v-if="textType === 'bold'" class="bold">
+      <p>{{ content }}</p>
+    </div>
+    <div v-else-if="textType === 'italic'" class="italic">
+      <p>{{ content }}</p>
+    </div>
+    <div v-else>
+      <p>{{ content }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RegularText',
+  props: {
+    textType: String,
+    content: String
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.bold
+  font-weight bold
+.italic
+  font-style italic
+</style>

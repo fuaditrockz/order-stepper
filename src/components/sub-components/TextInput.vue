86767fbff8b245524096a00addccaccba8ecc399
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-06-28 14:16:13 +0700

    [TextInput.vue] Create TextInput component.

---
+++ src/components/sub-components/TextInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-col cols="6">
+    <div>
+      <input type="text" id="email" :placeholder="placeholder">
+    </div>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: 'TextInput',
+  props: {
+    placeholder: String
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.col-6
+  padding 0
+  margin-bottom 10px
+div
+  width 92%
+  input
+    border 2px solid #ccc
+    padding 15px 15px
+    width 100%
+</style>

dbbbead229e7478ea85a154d4681d42bf313fea1
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-06-28 13:41:13 +0700

    [BackButton.vue] Create BackButton component.

---
+++ src/components/sub-components/BackButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <a href="">
+      <v-icon color="#535c68" size="20px">arrow_back</v-icon>
+      <h4>Back to {{ content }}</h4>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BackButton',
+  props: {
+    content: String
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+a
+  display inline-flex
+  justify-content space-between
+  width 137px
+  text-decoration none
+  padding 5px 12px 5px 5px
+  border-radius 5px
+  transition: 0.3s;
+  &:hover
+    background-color rgba(255, 138, 0, 0.2)
+  h4
+    color #535c68
+</style>

6fcdb385eef2cff9f35c9c26fefea2c9cbd9b628
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-06-29 12:56:28 +0700

    [Checkbox.vue] Separated checkbox component.

---
+++ src/components/atoms/CheckBox.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-checkbox v-model="checkbox" color="success" @click="check($event)">
+    <template v-slot:label>
+      <h4>{{ title }}</h4>
+    </template>
+  </v-checkbox>
+</template>
+
+<script>
+export default {
+  name: 'CheckBox',
+  props: {
+    title: String,
+    isChecked: Boolean
+  },
+  data () {
+    return {
+      checkbox: this.isChecked
+    }
+  },
+  methods: {
+    check: (e) => {
+      console.log(e.target.value)
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+h4
+  font-weight 500
+</style>

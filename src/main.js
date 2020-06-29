14eace233dffcf227520f87fe73fa4f01f8edb2c
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-06-29 13:23:21 +0700

    [Vuex] Initial vuex to the project.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
 import vuetify from './plugins/vuetify'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
@@ -7,6 +8,7 @@ Vue.config.devtools = true;
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/store/modules/cart.js
@@ -0,0 +1,35 @@
+const state = {
+  purchases_item: [
+    {
+      name: "Ps3 Slim Sony + Hdd 500gb + 2 Stick Warlles + Full Games",
+      price: 214800
+    },
+    {
+      name: "PAKET PODCAST 4 ORANG Mic Microphone BM800 BM 800 Mixer Recording",
+      price: 196500
+    },
+    {
+      name: "PEWIE TR-560-12 SPEAKER MEETING PORTABLE 12 INCH MIC WIRELESS - BLUETOOTH TR560",
+      price: 143920
+    }
+  ]
+}
+
+const getters = {
+
+}
+
+const actions = {
+
+}
+
+const mutations = {
+
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}

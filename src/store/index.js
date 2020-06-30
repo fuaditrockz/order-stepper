163949acaa0c83aecae398baac1269396863072f
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-06-30 15:25:54 +0700

    [store, stepper.js] Create store for stepper.

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,14 @@ import Vue from 'vue'
 
 import orders from './modules/orders'
 import extras from './modules/extras'
+import stepper from './modules/stepper'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     orders,
-    extras
+    extras,
+    stepper
   }
 })
---
+++ src/store/modules/stepper.js
@@ -0,0 +1,16 @@
+const state = {
+  stepper_position: 0
+}
+
+const getters = {}
+
+const actions = {}
+
+const mutations = {}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}

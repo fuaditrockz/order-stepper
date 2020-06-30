69cbef93370f8a489c901a817ac3126e69c58b2f
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-07-01 00:46:28 +0700

    [FinishOrder.vue] Finishing FinishOrder component.

--- src/components/organisms/FinishOrder.vue
+++ src/components/organisms/FinishOrder.vue
@@ -1,18 +1,30 @@
 <template>
   <div>
-    <v-row class="finish-content-wrapper mt-10">
-      <Title size="big" title="Thank you" />
-    </v-row>
+    <v-col class="finish-content-wrapper">
+      <Title size="big" title="Thank you" lineWidth="50%" class="mb-5" />
+      <v-col class="zero-padding">
+        <h4>Order ID: XXIB</h4>
+        <p class="mb-10">Your order will be delivered today with GO-SEND</p>
+        <BackButton content="Go to homepage" :click="goToHomePage" buttonWidth="220px" />
+      </v-col>
+    </v-col>
   </div>
 </template>
 
 <script>
 import Title from '../atoms/Title'
+import BackButton from '../atoms/BackButton'
 
 export default {
   name: 'FinishOrder',
   components: {
-    Title
+    Title,
+    BackButton
+  },
+  methods: {
+    goToHomePage() {
+      console.log('Hello World')
+    }
   }
 }
 </script>
@@ -20,4 +32,7 @@ export default {
 <style lang="stylus" scoped>
 .finish-content-wrapper
   min-height 400px
+  padding 150px
+  .title
+    width 200px
 </style>

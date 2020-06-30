f5060e6df32446fdd9ade49b04b7fea13f96c625
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-06-30 16:50:12 +0700

    [OptionCard.vue,  FormShipment.vue] Create component for option card and form for shipment.

---
+++ src/components/atoms/OptionCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-col cols="6" lg="3" class="shipment-item-wrapper zero-padding">
+    <a href="#" @click="onSelectThis()">
+      <div class="shipment-item">
+        <h4>{{ title }}</h4>
+        <h4>{{ price }}</h4>
+      </div>
+    </a>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: 'OptionCard',
+  props: {
+    title: String,
+    price: String,
+    isSelected: Boolean,
+    click: Function
+  },
+  methods: {
+    onSelectThis() {
+      this.click()
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+a
+  text-decoration none
+.shipment-item-wrapper
+  height 75px
+  padding 5px
+  .shipment-item
+    width 100%
+    height 100%
+    border 1.5px solid #ccc
+    border-radius 3px
+    padding 5px 15px
+    transition 0.3s
+    h4
+      color #828282
+      font-weight 600
+      transition 0.3s
+    &:hover > h4
+      color #2ed573
+    &:hover
+      border-color #2ed573
+</style>
---
+++ src/components/molecules/FormShipment.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-row>
+    <OptionCard
+      v-for="vendor in shipmentVendors"
+      :key="vendor.id"
+      :title="vendor.name"
+      :price="formattedNumber(vendor.price)"
+      :click="selectShipment"
+    />
+  </v-row>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import OptionCard from '../atoms/OptionCard'
+
+export default {
+  name: 'FormShipment',
+  components: {
+    OptionCard
+  },
+  computed: {
+    ...mapState({
+      shipmentVendors: state => state.extras.shipments
+    })
+  },
+  methods: {
+    selectShipment() {
+      console.log(this.shipmentVendors)
+    },
+    formattedNumber(number) {
+      return number.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")
+    },
+  }
+}
+</script>
